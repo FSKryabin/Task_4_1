ec0cc3c6b74c5707a69395731dcec125c2e1646c
FSKryabin <95165120+FSKryabin@users.noreply.github.com>
2022-02-16 16:31:32 +0300

    Добавьте файлы проекта.

---
+++ Task_4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0, e = 1;
+            Console.Write("Ввести целое число:");
+            int n = int.Parse(Console.ReadLine());
+            while (e <= n * 2)
+            {
+                s += e;
+                e += 2;
+                Console.WriteLine(s.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
